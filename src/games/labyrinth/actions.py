4e93382372baa6ede27e3967db77d146e6165754
Kai Plautz <kapl0001@stud.hs-kl.de>
2022-05-19 18:38:08 +0200

    Pauls Actions

--- src/games/labyrinth/actions.py
+++ src/games/labyrinth/actions.py
@@ -1,4 +1,5 @@
 from environment.action import Action
+from games.labyrinth.labyrinth import TileType
 
 
 class GoAction(Action):
@@ -15,3 +16,31 @@ class GoAction(Action):
         if result == 0:
             return -1
         return -5
+
+
+class MoveForwardAction(Action):
+    """Class representing the action to move the agent in a given direction"""
+
+    def __init__(self, id, game):
+        super().__init__(id, game)
+
+    def execute(self) -> int:
+        result = self.game.move_forward()
+        if result == TileType.BLOCKED:
+            return -1
+        elif result == TileType.EMPTY:
+            return 0
+        elif result == TileType.TARGET:
+            return 50
+
+
+class ChangeDirectionAction(Action):
+    """Class representing the action to move the agent in a given direction"""
+
+    def __init__(self, id, game, direction):
+        super().__init__(id, game)
+        self.direction = direction
+
+    def execute(self) -> int:
+        self.game.set_current_direction(self.direction)
+        return -2
