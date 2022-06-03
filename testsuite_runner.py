1ca1fc5e7da9188d66502612d3f4e756f412cc32
Kai Plautz <kapl0001>
2022-06-03 18:21:07 +0200

    copy suites

--- testsuite_runner.py
+++ testsuite_runner.py
@@ -1,4 +1,9 @@
 from deeplearning.testsuite.testsuite import TestSuiteMaze
+import tensorflow as tf
+
+physical_devices = tf.config.list_physical_devices('GPU')
+for device in physical_devices:
+    tf.config.experimental.set_memory_growth(device, True)
 
 
 def load_suite(filename):
