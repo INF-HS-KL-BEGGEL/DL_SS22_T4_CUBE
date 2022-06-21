b4a75838c8cca5958407e4442eafa339475ef288
Kai Plautz <kapl0001@stud.hs-kl.de>
2022-06-21 13:18:49 +0200

    Try Test on Skynet

--- src/deeplearning/agent/agent_qnetwork_optimized.py
+++ src/deeplearning/agent/agent_qnetwork_optimized.py
@@ -16,7 +16,8 @@ class QNetworkAgentOptimizd(Agent):
         self._state_size = len(environment.observation_space)
         self._action_size = len(environment.action_space)
 
-        self.experience_replay = collections.deque(maxlen=500)
+        self.episodes_in_history = 10
+        self.experience_replay = collections.deque(maxlen=timesteps_per_episode*self.episodes_in_history)
 
         # Initialize discount and exploration rate
         self.gamma = gamma
@@ -34,6 +35,137 @@ class QNetworkAgentOptimizd(Agent):
     def store(self, state, action, reward, next_state, terminated):
         """TODO"""
         self.experience_replay.append([state, action, reward, next_state, terminated])
+
+    def set_epsilon(self, epsilon):
+        self.epsilon = epsilon
+
+    def act(self, state):
+
+        if np.random.rand() <= self.epsilon:
+            return self.environment.get_random_action()
+
+        action, q_values = self.q_network.predict(state)
+        return action
+
+    def retrain(self, batch_size):
+        """TODO"""
+        #print(batch)
+        #print(batch[:, 0])
+        batch = np.array(self.experience_replay)
+
+        q_model = self.q_network.get_model()
+        t_model = self.target_network.get_model()
+        # Generate predictions for samples
+        states = batch[:, 0]
+        target = q_model.predict(states, steps=len(states))
+
+        rows_terminated = np.where(batch[:, 4] == True)
+        rewards_terminated_states = batch[rows_terminated][:, 2]
+        action_indizes = np.argmax(target[rows_terminated], axis=1)
+        target[:, action_indizes] = rewards_terminated_states
+
+        rows_not_terminated = np.where(batch[:, 4] == False)
+        rewards_not_terminated_states = batch[rows_not_terminated][:, 2]
+        next_states = batch[rows_not_terminated][:, 3]
+        t = t_model.predict(next_states, steps=len(next_states))
+
+        action_indizes = np.argmax(target[rows_not_terminated], axis=1)
+        target[:, action_indizes] = rewards_not_terminated_states + self.gamma * np.amax(t)
+        # Generate arg maxes for predictions
+        q_model.fit(states, target, epochs=self.episodes_in_history, steps_per_epoch=self.timesteps_per_episode, verbose=0)
+
+    def play(self, index):
+        """
+        """
+        state = self.environment.reset_environment()
+        sum_reward = 0
+        for timestep in range(self.timesteps_per_episode):
+            # Run Action
+            action = self.act(state)
+
+            # Take action
+            next_state, reward, terminated, info = self.environment.step(action)
+            sum_reward += reward
+
+            if terminated:
+                self.target_network.algin_model(self.q_network)
+                break
+
+            next_state_number = next_state.get_number()
+
+            self.store(state.get_number(), action.id, reward, next_state_number , terminated)
+            state = next_state
+
+        self.notify_writer_play((index, sum_reward))
+
+    def train(self, num_of_episodes, batch_size=100):
+        for e in range(0, num_of_episodes):
+            # Reset the enviroment
+            state = self.environment.reset_environment()
+
+            # Initialize variables
+            reward = 0
+            terminated = False
+            sum_reward = 0
+
+            #self.timesteps_per_episode = 1
+            for timestep in range(self.timesteps_per_episode):
+                # Run Action
+                action = self.act(state)
+                # Take action
+                next_state, reward, terminated, info = self.environment.step(action)
+                sum_reward += reward
+                #print(action, reward)
+                print(action)
+                next_state_number = next_state.get_number()
+                self.store(state.get_number(), action.id, reward, next_state_number, terminated)
+
+                if terminated:
+                    print("Terminated")
+                    self.target_network.algin_model(self.q_network)
+                    self.environment.reset_environment()
+                    break
+
+                state = next_state
+
+            if len(self.experience_replay) > batch_size:
+                #print("Retrain")
+                self.retrain(batch_size)
+
+            self.notify_writer_training((self.total_episodes, sum_reward))
+            print("Episode: {}, Reward {}".format(self.total_episodes, sum_reward))
+            self.total_episodes += 1
+
+class QNetworkAgentOptimizdOLD(Agent):
+
+    def __init__(self, environment, optimizer=Adam(learning_rate=0.01), epsilon=0.2, gamma=0.9, timesteps_per_episode=10):
+
+        super().__init__(environment)
+        # Initialize attributes
+        self._state_size = len(environment.observation_space)
+        self._action_size = len(environment.action_space)
+
+        self.experience_replay = collections.deque(maxlen=timesteps_per_episode*4)
+
+        # Initialize discount and exploration rate
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.timesteps_per_episode = timesteps_per_episode
+
+        self.total_episodes = 0
+
+        # Build networks
+        self.target_network = QNetwork(self._action_size, self._state_size, optimizer, self.environment)
+        self.q_network = QNetwork(self._action_size, self._state_size, optimizer, self.environment)
+
+        self.target_network.algin_model(self.q_network)
+
+    def store(self, state, action, reward, next_state, terminated):
+        """TODO"""
+        self.experience_replay.append([state, action, reward, next_state, terminated])
+
+    def set_epsilon(self, epsilon):
+        self.epsilon = epsilon
 
     def act(self, state):
 
@@ -66,11 +198,12 @@ class QNetworkAgentOptimizd(Agent):
         rewards_not_terminated_states = batch[rows_not_terminated][:, 2]
         next_states = batch[rows_not_terminated][:, 3]
         t = t_model.predict(next_states, steps=len(next_states))
+
         action_indizes = np.argmax(target[rows_not_terminated], axis=1)
         target[:, action_indizes] = rewards_not_terminated_states + self.gamma * np.amax(t)
 
         # Generate arg maxes for predictions
-        q_model.fit(states, target, batch_size=batch_size, verbose=0)
+        q_model.fit(states, target, epochs=4, steps_per_epoch=batch_size, verbose=0)
 
     def play(self, index):
         """
@@ -126,9 +259,9 @@ class QNetworkAgentOptimizd(Agent):
 
                 state = next_state
 
-                if len(self.experience_replay) > batch_size:
-                    #print("Retrain")
-                    self.retrain(batch_size)
+            if len(self.experience_replay) > batch_size:
+                #print("Retrain")
+                self.retrain(batch_size)
 
             self.notify_writer_training((self.total_episodes, sum_reward))
             print("Episode: {}, Reward {}".format(self.total_episodes, sum_reward))
--- test/unittests/agent_opimized_test.py
+++ test/unittests/agent_opimized_test.py
@@ -5,6 +5,7 @@ from deeplearning.agent.agent_qtable import QTableAgent
 from deeplearning.monitoring.monitoring import PlotWriter
 from deeplearning.games.labyrinth.labyrinth_game import LabyrinthGame
 from deeplearning.games.labyrinth.maze_game_gui_adapter import LabyrinthGameGuiAdapter
+from deeplearning.games.cube import EnvCube, CubeGame
 
 from deeplearning.games.labyrinth.env_labyrinth import EnvLabyrinth
 from tensorflow.keras.optimizers import Adam
@@ -12,50 +13,54 @@ from tensorflow.keras.optimizers import Adam
 class TestQNetworkAgentOptimizd(unittest.TestCase):
 
     def test(self):
-        maze_game = LabyrinthGameGuiAdapter(LabyrinthGame.setup_game(6, 6, 4, 12))
+        maze_game = LabyrinthGameGuiAdapter(LabyrinthGame.setup_game(10, 10, 5, 12))
         #maze_game = LabyrinthGame.setup_game(7, 7, 4, 12)
 
         env = EnvLabyrinth(maze_game)
-        eps = 0.2
-        timesteps_per_episode = 100
-        learning_rate = 0.05
-        agent = QNetworkAgentOptimizd(env, optimizer=Adam(learning_rate=learning_rate), timesteps_per_episode=timesteps_per_episode, epsilon=0.2, gamma=0.9)
+
+        eps = 0.1
+        timesteps_per_episode = 75
+        learning_rate = 0.02
+        agent = QNetworkAgentOptimizd(env, optimizer=Adam(learning_rate=learning_rate), timesteps_per_episode=timesteps_per_episode, epsilon=eps, gamma=0.95)
 
         train_plot = PlotWriter("Training", plot_information="LearningRate: %s, TimeStepsPerEpisode:%s, Epsilon: %s" % (learning_rate, timesteps_per_episode, eps),
                                 should_render=True)
         play_plot = PlotWriter("Play", plot_information="LearningRate: %s, TimeStepsPerEpisode:%s, Epsilon: %s" % (learning_rate, timesteps_per_episode, eps),
                                 should_render=True)
 
+
         agent.register_writer_training(train_plot)
         agent.register_writer_play(play_plot)
 
         for i in range(0, 50):
-            agent.train(20, batch_size=50)
+            agent.train(100, batch_size=40)
             agent.play(i)
+            agent.set_epsilon(eps)
 
+    def test_cube(self):
 
-    # def test_qtable(self):
-    #     maze_game = LabyrinthGameGuiAdapter(LabyrinthGame.setup_game(10, 10, 2, 12))
-    #     #maze_game = LabyrinthGame.setup_game(7, 7, 4, 12)
+        env = EnvCube(CubeGame.setup_game(3))
 
-    #     env = EnvLabyrinth(maze_game)
-    #     #agent = QNetworkAgentOptimizd(env, optimizer=Adam(learning_rate=0.01), timesteps_per_episode=250, epsilon=0.4, gamma=0.99)
-    #     agent = QTableAgent(env, epsilon=0.4, gamma=0.99)
+        eps = 0.2
+        timesteps_per_episode = 2000
+        learning_rate = 0.01
+        agent = QNetworkAgentOptimizd(env, optimizer=Adam(learning_rate=learning_rate),
+                                      timesteps_per_episode=timesteps_per_episode, epsilon=eps, gamma=0.95)
 
-    #     train_plot = PlotWriter("Training")
-    #     train_plot.set_label("Epoche", "Reward")
+        train_plot = PlotWriter("Training", plot_information="LearningRate: %s, TimeStepsPerEpisode:%s, Epsilon: %s" % (
+        learning_rate, timesteps_per_episode, eps),
+                                should_render=True)
+        play_plot = PlotWriter("Play", plot_information="LearningRate: %s, TimeStepsPerEpisode:%s, Epsilon: %s" % (
+        learning_rate, timesteps_per_episode, eps),
+                               should_render=True)
 
-    #     play_plot = PlotWriter("Play")
-    #     play_plot.set_label("Epoche", "Reward")
+        agent.register_writer_training(train_plot)
+        agent.register_writer_play(play_plot)
 
-    #     agent.register_writer_training(train_plot)
-    #     agent.register_writer_play(play_plot)
-
-    #     for i in range(0, 50):
-    #         agent.train(50)
-    #         agent.play(i)
-
-
+        for i in range(0, 50):
+            agent.train(100, batch_size=40)
+            agent.play(i)
+            agent.set_epsilon(eps)
 
 
 if __name__ == '__main__':
